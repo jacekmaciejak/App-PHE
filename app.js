606e8439ac09082e0a7cfb632f031fcb685eeef6
Jacek Maciejak <jacekmaciejak@poczta.fm>
2020-04-22 17:07:32 +0200

    Income and Expense func constructors

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
 //BUDGET CONTROLLER
 const budgetController = (function () {
-  //code
+  //Constructor-object,
+  const Expense = function (id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  const Income = function (id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  const data = {
+    allItems: {
+      exp: [],
+      inc: [],
+    },
+    totals: {
+      exp: 0,
+      inc: 0,
+    },
+  };
 })();
 
 //UI CONTROLLER
