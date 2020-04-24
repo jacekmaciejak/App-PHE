2fa8b0fe89b5c4208c4e7cd5f8590aee238edde0
Jacek Maciejak <jacekmaciejak@poczta.fm>
2020-04-24 13:57:16 +0200

    Added the item to the budget controller

--- app.js
+++ app.js
@@ -23,6 +23,32 @@ const budgetController = (function () {
       inc: 0,
     },
   };
+
+  return {
+    //des:description, val:value
+    addItem: function (type, des, val) {
+      let newItem, ID;
+      //Create new ID
+      if (data.allItems[type].length > 0) {
+        ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+      } else {
+        ID = 0;
+      }
+      //Create new item based on 'inc' or 'exp' type
+      if (type === "exp") {
+        newItem = new Expense(ID, des, val);
+      } else if (type === "inc") {
+        newItem = new Income(ID, des, val);
+      }
+      //Push it into our data structure
+      data.allItems[type].push(newItem);
+      //Return the new element
+      return newItem;
+    },
+    testing: function () {
+      console.log(data);
+    },
+  };
 })();
 
 //UI CONTROLLER
@@ -67,10 +93,17 @@ const controller = (function (budgetController, UIController) {
 
   //Get value from input
   const ctrlAddItem = function () {
+    let input, newItem;
+
     //1. Get the field input data
-    const input = UIController.getInput();
+    input = UIController.getInput();
 
     //2. Add the item to the budget controller
+    newItem = budgetController.addItem(
+      input.type,
+      input.description,
+      input.value
+    );
     //3. Add the item to the UI
     //4. Calculate the budget
     //5. Display the budget on the UI
